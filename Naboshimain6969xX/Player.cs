5447cd451b4053096b039474827a175b4a4134c6
AIE\s171811 <thomas.kleemann@yahoo.com.au>
2017-04-03 19:02:30 +0930

    new platformer game

---
+++ Naboshimain6969xX/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Naboshimain6969xX
+{
+    class Player
+    {
+        Sprite sprite = new Sprite();
+
+        public Player()
+        {
+        }
+        public void Load(ContentManager content)
+        {
+            sprite.Load(content, "hero.png");
+        }
+
+        public void Update(float deltaTime)
+        {
+            sprite.Update(deltaTime);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch);
+        }
+    }
+}
